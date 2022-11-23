2a5661fe81f0f3c875f80550ec6dff87555be872
vickyxie0906 <vickyxie0906@gmail.com>
2022-11-23 13:53:30 +0800

    動態資料選擇

--- project5/datasource.py
+++ project5/datasource.py
@@ -2,7 +2,7 @@ import requests
 api_key="c017ac798470580402a9cebf01171858"
 cityName="Taipei"
 
-def get_forcast_data():
+def get_forcast_data(cityName):
     url=f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&appid={api_key}&lang=zh_tw&units=metric"
 
     response = requests.get(url=url)
