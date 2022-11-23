ae99fe7fbad75f271e1ad2b65799cbeede990c40
vickyxie0906 <vickyxie0906@gmail.com>
2022-11-23 11:19:55 +0800

    main執行

--- project5/datasource.py
+++ project5/datasource.py
@@ -1,1 +1,15 @@
+import requests
 api_key="c017ac798470580402a9cebf01171858"
+cityName="Taipei"
+
+def get_forcast_data():
+    url=f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&appid={api_key}&lang=zh_tw&units=metric"
+
+    response = requests.get(url=url)
+
+
+    if response.ok:
+        print("下載成功")
+        print(response.text)
+    else:
+        print("下載失敗")
--- project5/main.py
+++ project5/main.py
@@ -2,6 +2,7 @@ import datasource
 
 def main():
     print("這裡是MAIN FUNCTION")
+    datasource.get_forcast_data()
 
 if __name__ =="__main__":
     print("這裡是程式的執行點")
