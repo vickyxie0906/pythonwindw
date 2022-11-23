56e6c6913021cb4b46ddf60d560b159e2788a42a
vickyxie0906 <vickyxie0906@gmail.com>
2022-11-23 11:40:29 +0800

    json轉python

--- project5/main.py
+++ project5/main.py
@@ -2,7 +2,8 @@ import datasource
 
 def main():
     print("這裡是MAIN FUNCTION")
-    datasource.get_forcast_data()
+    all_data=datasource.get_forcast_data()
+    print(type(all_data))
 
 if __name__ =="__main__":
     print("這裡是程式的執行點")
