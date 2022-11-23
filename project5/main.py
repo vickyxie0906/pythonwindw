1fa7290e1c6f522dfa7747edfef713ad89d4bff9
vickyxie0906 <vickyxie0906@gmail.com>
2022-11-23 11:56:50 +0800

    抓list 時間點

--- project5/main.py
+++ project5/main.py
@@ -2,8 +2,10 @@ import datasource
 
 def main():
     print("這裡是MAIN FUNCTION")
-    all_data=datasource.get_forcast_data()
-    print(type(all_data))
+    list_data=datasource.get_forcast_data()
+    for item in list_data:
+        print(item["dt_txt"])
+    # print(type(list_data))
 
 if __name__ =="__main__":
     print("這裡是程式的執行點")
