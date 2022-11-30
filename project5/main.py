1be4a609437b1b02d96113433ac2f174925e9bab
vickyxie0906 <vickyxie0906@gmail.com>
2022-11-30 09:49:07 +0800

    items引出KEY VALUE

--- project5/main.py
+++ project5/main.py
@@ -14,9 +14,10 @@ class Window(tk.Tk): #繼承TK類別
         buttons_frame.pack(padx=50,pady=(0,30)) #左右 下上 間距
         #設定GRID的ROW數量
         grid_row_nums=3
-        for index, key in enumerate(cities_dict):  # enumerate會回傳索引值 將數據組合為索引序列 一般用在迴圈
+        for index, cities in enumerate(cities_dict.items()):  # enumerate會回傳索引值 將數據組合為索引序列 一般用在迴圈
             # print(index,key)
-            tk.Button(buttons_frame, text=key, font=("Arial", 15),
+            cname,ename=cities
+            tk.Button(buttons_frame, text=f"{cname}{ename}", font=("Arial", 15),
                       padx=20, pady=3).grid(row=index % grid_row_nums, column=index//grid_row_nums)
             # tk.Button(buttons_frame, text=key, font=("Arial",15),padx=20, pady=3).grid(row=index % 3, column=index//3)
             # 建立按鈕, 字體 15 寬20 高3. grid 網格容器
